86cd3377c12b323e0ff15f56a214fa229a20fbe8
kay2-dev <odufotekayode4@gmail.com>
2026-01-08 16:03:58 +0100

    db: configuration

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core';
 
 export const usersTable = mysqlTable('users_table', {
-    id: serial().primaryKey(),
+    id: int().primaryKey(),
     name: varchar({ length: 255 }).notNull(),
     age: int().notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
